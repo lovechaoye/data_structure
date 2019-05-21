3bda77add040ff40feecfd7db21bf0c5c6585a3a
lichao <274121678@qq.com>
2019-05-22 01:35:29 +0800

    线性表的顺序表示和实现

---
+++ 线性表/线性表的顺序实现/SqList.cpp
@@ -0,0 +1,85 @@
+#include "SqList.h"
+void InitSqList(pSqList L){//初始化一个空的线性表
+    L->elem=(pSqList)malloc(sizeof(ElemType)*List_Size);
+    if (!elem)
+    {
+        exit(EXIT_FAILURE);
+    }
+    L->length=0;
+    L->listsize=List_Size;
+}
+
+void CreateSqList(pSqList L,int n){
+
+    int i=0;
+    while (i<n)
+    {
+       scanf("%d",L->elem+n);
+        L->length++;
+        i++;
+    }
+}
+
+void DestroySqList(pSqList L){
+    free(L->elem);//注意:free和malloc必须一一对应,不要free非malloc分配的指针
+}
+
+int SqListInsert(pSqList L,int index,ElemType x){
+    ElemType * old;
+    if (index<0||index>=L->length)//插入位置不合法
+    {
+        return ERROR;
+    }
+    if (L->length>=L->listsize)//当前存储空间已满,增加存储空间
+    {
+        old=L->elem;
+        if((L->elem=(ElemType*)realloc(L->elem,sizeof(ElemType)*(L->listsize+ListIncrement)))==NULL){
+            free(old);
+            return ERROR;   
+        }
+        L->listsize=L->listsize+ListIncrement;
+    }
+    for (size_t i = L->length; i >index ; i--)//插入位置之后的元素一次后移
+    {
+        *(L->elem+i)=*(L->elem+i-1);
+    }
+    *(L->elem+index)=x;//插入元素
+    L->length++;//表长增1
+}
+
+int SqListDelete(pSqList L,int index){
+    if (index<0||index>=L->length)//删除位置不合法
+    {
+        return ERROR;
+    }
+    for (size_t i =index; i < L->length-1; i++)//删除位置之后的元素依次前移
+    {
+        *(L->elem+i)=*(L->elem+i+1);
+    }
+    L->length--;//表长减一
+    return OK;
+}
+
+int LocateElem(pSqList L,ElemType x){
+    int i=0;
+    while (i<L->length&&x!=*(L->elem+i)) i++;
+    if (i<L->length)
+    {
+        return i;
+    }
+    else
+    {
+       return -1;
+    }
+}
+
+void DispSqList(pSqList L){
+    int i=0;
+    while (i < L->length)
+    {
+         printf("%d\n",*(L->elem+i);
+         i++;
+    }
+}
+
+
---
+++ 线性表/线性表的顺序实现/SqList.h
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+//线性表的顺序表示与实现
+typedef int ElemType;
+#define List_Size 100//线性表存储空间的初始化分配量
+#define ListIncrement 10//线性表存储空间的分配增量
+#define ERROR 0
+#define OK 1
+typedef struct {
+    ElemType *elem;//存放线性表的数组基地址
+    int length;//线性表的当前长度
+    int listsize;//线性表当前分配的存储容量
+}SqList,*pSqList;
+
+//顺序表的初始化
+void InitSqList(pSqList L);
+
+//建立顺序表
+void CreateSqList(pSqList L,int n);
+
+//销毁顺序表
+void DestroySqList(pSqList L);
+
+//插入操作
+int SqListInsert(pSqList L,int i,ElemType x);
+
+//删除操作
+int SqListDelete(pSqList L,int i);
+
+//按值查找
+int LocateElem(pSqList L,ElemType x);
+
+//一次输出线性表中的所有元素
+void DispSqList(pSqList L);
+
